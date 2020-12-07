37cce47591b08c978e1ae7ef666f478d74fd736c
Mostafa Adel <mostafa.adel@dbsmena.com>
2020-12-07 16:19:26 +0200

    add configuration for entities

---
+++ Src/Infrastructure/IssueTracker.Infrastructure/Persistence/Configurations/TodoItemConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using IssueTracker.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IssueTracker.Infrastructure.Persistence.Configurations
+{
+    public class TodoItemConfiguration : IEntityTypeConfiguration<TodoItem>
+    {
+        public void Configure(EntityTypeBuilder<TodoItem> builder)
+        {
+            //builder.Ignore(e => e.DomainEvents);
+
+            //builder.Property(t => t.Title)
+            //    .HasMaxLength(200)
+            //    .IsRequired();
+        }
+    }
+}
